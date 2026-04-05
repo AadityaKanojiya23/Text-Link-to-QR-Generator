9d674607cf09c6ec5066ed2d6d3111224e4993fd
AdityaKanojiya23 <adityastudent750@gmail.com>
2026-04-05 22:09:32 +0530

    msg

---
+++ script.js
@@ -0,0 +1,12 @@
+let intputext = document.querySelector(".intputext");
+let inputbox = document.querySelector("#inputbox");
+let inputbtn = document.querySelector("#inputbtn");
+let qrImage = document.querySelector("#qrImage");
+
+inputbtn.addEventListener('click', () => {
+    if(inputbox.value.trim() === ""){
+        alert("Please enter text or URL");
+    } else {
+        qrImage.src = "https://quickchart.io/qr?text=" + encodeURIComponent(inputbox.value);
+    }
+});
